8de5deb9b19aad7bd4e346312939d74a5656f3cf
Shrijan <justforyouutubee@gmail.com>
2024-10-14 14:56:16 +0530

    Something new

---
+++ Queue/Implementation/queue_using_ll.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    Node* next;
+    int data;
+    Node(int data1){
+        next=nullptr;
+        data=data1;
+    }
+};
+class Queue{
+    
+};
